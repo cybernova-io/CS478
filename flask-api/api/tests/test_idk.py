2c4be3559313239d574b42eee43aa787b86195a2
KX61bxFCws9ZxoBDRWos <jrbbrawner@gmail.com>
2022-09-01 20:19:46 -0500

    working on setting up tests

---
+++ flask-api/api/tests/test_idk.py
@@ -0,0 +1,21 @@
+import os
+
+import unittest
+from flask import current_app
+from api import create_app
+
+
+class TestWebApp(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.appctx = self.app.app_context()
+        self.appctx.push()
+        
+    def tearDown(self):
+        self.appctx.pop()
+        self.app = None
+        self.appctx = None
+
+    def test_app(self):
+        assert self.app is not None
+        assert current_app == self.app
