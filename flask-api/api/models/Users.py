7a68a8ee76d34ceba9e8df93c7cb6c72ca01f082
KX61bxFCws9ZxoBDRWos <jrbbrawner@gmail.com>
2022-09-01 19:16:25 -0500

    Added suport for notifications

--- flask-api/api/models/Users.py
+++ flask-api/api/models/Users.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from sqlalchemy import create_engine
 from flask import current_app as app
 from .Messages import Message
+from .Notifications import Notification
+import json
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
@@ -57,6 +59,9 @@ class User(UserMixin, db.Model):
                                         backref='recipient', lazy='dynamic')
 
     last_message_read_time = db.Column(db.DateTime)
+
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
     
     
     def set_password(self, password):
@@ -127,6 +132,12 @@ class User(UserMixin, db.Model):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
 
     def serialize(self):
         return {
