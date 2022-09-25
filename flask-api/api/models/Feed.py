13de64cf0cce64e7cd03f5cbdd065f7b561f639e
jrbrawner <jrbbrawner@gmail.com>
2022-09-24 20:19:54 -0500

    initial

---
+++ flask-api/api/models/Feed.py
@@ -0,0 +1,12 @@
+from .. import db
+
+class Feed(db.Model):
+    """Model for the feed."""
+
+    __tablename__ = "Feed"
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.Integer, db.relationship('User.feed'), backref='owner', lazy=True)
+    
+
+
