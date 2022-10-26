ec5cf4bcc85aeb8bd8406f189ba8016ed74e1818
John Messmer <jmessmer37@gmail.com>
2022-10-25 20:52:30 -0500

    feed implementtaion

--- flask-api/api/routes/feed.py
+++ flask-api/api/routes/feed.py
@@ -1,21 +1,34 @@
 from flask import Blueprint
 from flask_login import current_user, login_required
 import os
+import json
 from api.models.db import db
-from ..models.Feed import Feed
+#from ..models.Feed import Feed
 from ..models.Users import User
 from ..models.Posts import Post,PostComment,PostLike
-
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    flash,
+    request,
+    session,
+    url_for,
+    jsonify,
+    Response,
+)
+from ..services.WebHelpers import WebHelpers
 
 feed_bp = Blueprint("feed_bp", __name__)
+
+
 
 @feed_bp.get("/api/feed")
 @login_required
 def display_user_feed():
-    #data = {}
-    friend = current_user.friend
-    if current_user.is_friend():
-        user_feed = Post.query.all()
+    user_feed = {}
+    friends = current_user.friend
+    for i in friends:
         user_feed.append([x.serialize() for x in i.posts])
     return user_feed
 
